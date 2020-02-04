abb6f7fd282fdea3337c1eaf0c8e6e9baa0ff4a2
김종욱 <kimJoungUk@gimjong-ug-ui-MacBook-Pro.local>
2020-02-04 20:58:47 +0900

    상품 가격및 할인 view 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import response from "./data.json"
-import { ImageTap, ProductDetail, ProductList, SelectOptions, BottomDetail } from './components/index'
+import { ProductDetail, ProductList, SelectOptions, BottomDetail } from './components/index'
 import ImageTapContainer from './containers/ImageTapContainer';
 
 import styles from './App.scss'
@@ -9,7 +9,7 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 function App() {
-  const { partner, images } = response
+  const { partner, images, name, price, discount_price, discount_rate } = response;
   console.log(response)
   return (
     <div className="main">
@@ -18,7 +18,7 @@ function App() {
           <ImageTapContainer subImages={images}/> 
         </div>
         <div className="right">
-          right
+          <ProductDetail name={name} price={price} discount_price={discount_price} discount_rate={discount_rate} />
         </div>        
       </div>
       <div>
--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from './ProductDetail.scss'
+import classNames from 'classnames/bind';
 
-function ProductDetail() {
+const cx = classNames.bind(styles);
+
+function numberFormat(inputNumber) {
+  return inputNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function ProductDetail({ name, price, discount_price, discount_rate }) {
   return (
-    <div>
-      상품에 대한 설명
+    <div className={cx('product-detail')}>
+      <div className={cx('detail-title')}>
+        <span className={cx('title-name')}>{name}</span>
+        <span className={cx('title-free-box')}>무료배송</span>
+      </div>
+      <div className={cx('detail-price')}>
+        <p className={cx('price-full')}>{numberFormat(price)}원</p>
+        <span className={cx('price-discount-price')}>{numberFormat(discount_price)}원</span>
+        <span className={cx('price-discount-rate')}>{discount_rate}</span>
+      </div>
+      <hr />
     </div>
   )
 }
